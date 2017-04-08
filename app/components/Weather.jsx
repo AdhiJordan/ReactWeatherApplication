a5d06f1047bd73afc362e8105202c3be47e9fa8a
Adhithya <adhithyaprabhu77@gmail.com>
2017-04-08 15:50:02 +0530

    Adding loaders

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,17 +1,53 @@
 var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
+var openWeatherMap = require('openWeatherMap');
+
 var Weather = React.createClass({
+	getInitialState: function() {
+		return {
+			isLoading: false
+		}
+	},
+	handleSearch: function(location) {
+		var that = this;
+		this.setState({isLoading: true})
+		openWeatherMap.getTemp(location).then(function(temp){
+			that.setState({
+					location: location,
+					temp: temp
+			});
+		}, function(errorMessage) {
+			alert('hai');
+			alert(errorMessage);
+		});
+		// this.setState({
+		// 	location: location,
+		// 	temp: 23
+		// });
+	},
 	render: function() {
+		var {temp, location, isLoading} = this.state;
+
+		function renderMessage () {
+			if(isLoading) {
+				return <h3>Fetching the results ....</h3>;
+				}
+				else if(temp && location) {
+					return <WeatherMessage temp={temp} location={location}/>;
+				}
+
+		}
+
 			return (
-<div>
+				<div>
 				<h2>Weather component Via Cities</h2>
-				<WeatherForm></WeatherForm>
-				<WeatherMessage />
+				<WeatherForm onSearch={this.handleSearch}/>
+				{renderMessage()}
 				
 				</div>
 				);
-	}
+		}
+		
 });
-
 module.exports = Weather;
